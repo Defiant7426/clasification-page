21aa7986e219ab43f852de7f921f572df309791f
Defiant7426 <pldelacruzv@gmail.com>
2024-09-24 23:37:51 -0500

    creando el encabezado

--- clasification-page/src/App.tsx
+++ clasification-page/src/App.tsx
@@ -3,7 +3,12 @@ function App() {
 
   return (
     <>
-      <h1>Detector de gatos</h1>
+      <div className="bg-red-500 p-4 text-center">
+        <p className="text-2xl font-black">
+           Detector de gatos y perros.<span className="text-white font-black"> v1.0</span>
+        </p>
+      </div>
+      <h1 className="text-2xl font-bold mt-4">Detector de gatos</h1>
     </>
   )
 }
