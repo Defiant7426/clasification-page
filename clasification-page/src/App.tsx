d7f439b2eb56e436e4ec7767cfec8230e81bf51d
Defiant7426 <pldelacruzv@gmail.com>
2024-09-26 21:22:21 -0500

    Agregando cuadro para subir imagenes

--- clasification-page/src/App.tsx
+++ clasification-page/src/App.tsx
@@ -2,10 +2,23 @@ import { useState } from "react";
 
 function App() {
   const [activeButton, setActiveButton] = useState<string | null>(null);
+  const [selectedFiles, setSelectedFiles] = useState<FileList | null>(null);
 
   const handleClick = (buttonName: string) => {
     setActiveButton(buttonName); 
     console.log(`Botón presionado: ${buttonName}`);
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files) {
+      console.log(files);
+    }
+  }
+
+  const handleDivClick = () => {
+    const fileInput = document.getElementById("file") as HTMLInputElement;
+    fileInput.click();
   };
 
   return (
@@ -37,9 +50,38 @@ function App() {
         </button>
       </div>
 
-      <h1 className="text-center texxt-2xl font-bold mt-4">
+      <h1 className="text-center text-2xl font-bold mt-4">
         Detector usando: {activeButton ? activeButton : "Ningún modelo seleccionado"}
       </h1>
+      
+      <div className="flex justify-center mt-4">
+        <div
+          className="file-upload border-2 border-dashed border-gray-400 rounded-lg p-6 w-full max-w-md text-center bg-gray-100 hover:bg-gray-200 cursor-pointer"
+          onClick={handleDivClick}
+        >
+          <p className="text-gray-600">Arrastra y suelta tu archivo aquí o haz clic para seleccionar</p>
+          <input
+            type="file"
+            id="file"
+            multiple
+            className="hidden"
+            onChange={handleFileChange}
+          />
+        </div>
+      </div>
+
+      {selectedFiles && (
+        <div className="flex justify-center mt-4">
+          <ul>
+            {Array.from(selectedFiles).map((file, index) => (
+              <li key={index} className="text-center">
+                {file.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
     </>
   );
 }
